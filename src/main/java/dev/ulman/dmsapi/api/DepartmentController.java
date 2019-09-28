45cc36d1063e473018c243824d19702a344b9237
Marcin Ulman <marcin@ulman@dev>
2019-09-28 22:04:43 +0200

    add microservice connection settings

--- src/main/java/dev/ulman/dmsapi/api/DepartmentController.java
+++ src/main/java/dev/ulman/dmsapi/api/DepartmentController.java
@@ -9,13 +9,12 @@ import dev.ulman.dmsapi.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RequestMapping("departments")
-@Controller
+@RestController
 public class DepartmentController {
 
     private final DepartmentService departmentService;
